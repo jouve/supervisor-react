54f12d0fbffd8101a9888254f823ed427e1247c0
Cyril Jouve <jv.cyril@gmail.com>
2021-02-17 23:54:57 +0100

    misc

---
+++ react-app/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+if ! test -w /var/run/docker.sock; then
+  SUDO=sudo
+else
+  SUDO=
+fi
+
+if docker container inspect cache_cache_1 &>/dev/null; then
+  cache=--volumes-from=cache_cache_1
+else
+  cache=
+fi
+
+docker volume rm node_modules || true
+docker volume create node_modules
+$SUDO docker run \
+  $cache \
+  -v "$(readlink -f "$(dirname "$0")")":/srv \
+  -v node_nodumes:/srv/node_modules \
+  -w /srv \
+  alpine:3.13.0 \
+  sh -x -c '
+set -e
+apk add --no-cache alpine-conf
+setup-apkcache /var/cache/apk
+apk add --no-cache make nodejs yarn python2
+yarn install
+yarn build
+'
