052b1538f710f6e803917f0c91a00b935586ad4c
Cyril Jouve <jv.cyril@gmail.com>
2021-04-12 23:52:42 +0200

    bump

--- update.sh
+++ update.sh
@@ -16,7 +16,7 @@ $SUDO docker run \
   $cache \
   -v "$(readlink -f "$(dirname "$0")")":/srv \
   -w /srv \
-  alpine:3.13.2 sh -x -c '
+  alpine:3.13.4 sh -x -c '
 set -e
 apk add --no-cache alpine-conf
 setup-apkcache /var/cache/apk
